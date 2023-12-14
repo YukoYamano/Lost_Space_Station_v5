23acd907d346017831ce2b12644502788f9b62f4
YukoYamano <yyamano@my.centennialcollege.ca>
2023-12-14 09:54:22 -0500

    First commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "FrostFire Gaming-Lost_Space_Station-0.5";
+const contentToCache = [
+    "Build/v5.loader.js",
+    "Build/v5.framework.js.unityweb",
+    "Build/v5.data.unityweb",
+    "Build/v5.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
